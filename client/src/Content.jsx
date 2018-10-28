1635e84404e9592b894fc3c4c81357c537f89ee7
BoyWithSilverWings <agney@outlook.in>
2018-10-28 22:52:38 +0530

    add note listing

--- client/src/Content.jsx
+++ client/src/Content.jsx
@@ -1,24 +1,58 @@
 import React, { Component } from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Skeleton } from "antd";
 
+import Note from "./Note";
 import api from "./api";
 
 class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: [],
+    };
+  }
   componentDidMount() {
     this.getAllNotes();
   }
   getAllNotes = () => {
+    this.setState({
+      loading: true,
+    });
     api.notes
       .list()
-      .then(console.log, console.log)
-      .catch(console.log);
+      .then(response => {
+        this.setState({
+          loading: false,
+          data: response.data.result,
+        });
+      })
+      .catch(console.error)
+      .finally(() => {
+        this.setState({
+          loading: false,
+        });
+      });
   };
   render() {
+    if (this.state.loading) {
+      return (
+        <Row>
+          {[1, 2, 3].map(() => (
+            <Col span={8}>
+              <Skeleton active />
+            </Col>
+          ))}
+        </Row>
+      );
+    }
     return (
       <Row>
-        <Col span={8}>col-8</Col>
-        <Col span={8}>col-8</Col>
-        <Col span={8}>col-8</Col>
+        {this.state.data.map(item => (
+          <Col span={8}>
+            <Note title={item.title} text={item.text} />
+          </Col>
+        ))}
       </Row>
     );
   }
