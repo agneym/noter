b033e1ea879c976bcb72a08d64306c10d0e605ad
BoyWithSilverWings <agney@outlook.in>
2018-10-28 09:15:53 +0530

    remove deprecation warning for sequelize

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -4,15 +4,9 @@ const Sequelize = require("sequelize");
  * Connect to Postgres Database.
  */
 async function connectDb() {
-  const sequelize = new Sequelize("postgres://agney:@127.0.0.1:5432/noter");
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log("Connection has been established successfully.");
-    })
-    .catch(err => {
-      console.error("Unable to connect to the database:", err);
-    });
+  const sequelize = new Sequelize("postgres://agney:@127.0.0.1:5432/noter", {
+    operatorsAliases: false
+  });
   return sequelize;
 }
 
