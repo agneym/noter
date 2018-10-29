78077c7b2e18829b7726de7799f5cbb46291c85b
BoyWithSilverWings <agney@outlook.in>
2018-10-29 08:55:19 +0530

    documentation comments for list and get one

--- client/src/api/notes/list.js
+++ client/src/api/notes/list.js
@@ -1,9 +1,13 @@
 import axios from "../axios";
 
+/**
+ * Get a list of all notes
+ * @returns {Promise} Promise object containing notes and message in data
+ */
 function list() {
   return axios({
     method: "GET",
-    url: "/notes"
+    url: "/notes",
   });
 }
 
