01dd6f58d70637fe6c413c2485d99d9b25b1e63a
BoyWithSilverWings <agney@outlook.in>
2018-10-29 08:56:40 +0530

    update API docs

--- client/src/api/notes/update.js
+++ client/src/api/notes/update.js
@@ -1,13 +1,20 @@
 import axios from "../axios";
 
+/**
+ * Create a new Note
+ * @param {number} id - Unique ID of note
+ * @param {string} title - Title of the note
+ * @param {string} text - Complete text of the note
+ * @returns {Promise} Promise object containing the updated note and message in data
+ */
 function update(id, title, text) {
   return axios({
     method: "POST",
     url: `/notes/${id}`,
     data: {
       title,
-      text
-    }
+      text,
+    },
   });
 }
 
