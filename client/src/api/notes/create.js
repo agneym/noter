09f4f7a5d2208ab499e29bf9b87702464f42f196
BoyWithSilverWings <agney@outlook.in>
2018-10-29 08:53:54 +0530

    Create JSDoc for create

--- client/src/api/notes/create.js
+++ client/src/api/notes/create.js
@@ -1,13 +1,19 @@
 import axios from "../axios";
 
+/**
+ * Create a new Note
+ * @param {string} title - Title of the note
+ * @param {string} text - Complete text of the note
+ * @returns {Promise} Promise object containing the created note and message in data
+ */
 function create(title, text) {
   return axios({
     method: "POST",
     url: "/notes",
     data: {
       title,
-      text
-    }
+      text,
+    },
   });
 }
 
