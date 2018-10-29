169baa44ae7df6a4d0fd76035162b998a7cd230a
BoyWithSilverWings <agney@outlook.in>
2018-10-29 08:56:06 +0530

    docs for remove API

--- client/src/api/notes/remove.js
+++ client/src/api/notes/remove.js
@@ -1,9 +1,14 @@
 import axios from "../axios";
 
+/**
+ * Delete a note
+ * @param {number} id - Unique ID of note
+ * @returns {Promise} Promise object containing the deleted note and message in data
+ */
 function remove(id) {
   return axios({
     method: "DELETE",
-    url: `/notes/${id}`
+    url: `/notes/${id}`,
   });
 }
 
