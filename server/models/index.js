e93c54d88e72e0ffcf56c6f299ccfcd509583e67
BoyWithSilverWings <agney@outlook.in>
2018-10-29 17:55:51 +0530

    Add sequelize history

--- server/models/index.js
+++ server/models/index.js
@@ -4,8 +4,11 @@ const sequelize = new Sequelize(`postgres://agney:@127.0.0.1:5432/noter`, {
   operatorsAliases: false
 });
 
+const { Note, NoteHistory } = sequelize.import("./Note");
+
 const models = {
-  Note: sequelize.import("./Note")
+  Note,
+  NoteHistory
 };
 
 Object.keys(models).forEach(key => {
